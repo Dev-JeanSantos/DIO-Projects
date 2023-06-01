fc7a352ba5e5c65be6fc2c4cb78ad731ac2b9794
Dev-JeanSantos <jeancbsan@gmail.com>
2023-06-01 20:58:53 -0300

    implemented getAll and delete customer controller

--- credit-application-system/src/main/kotlin/com/fourtk/creditapplicationsystem/controllers/CustomerController.kt
+++ credit-application-system/src/main/kotlin/com/fourtk/creditapplicationsystem/controllers/CustomerController.kt
@@ -1,7 +1,12 @@
 package com.fourtk.creditapplicationsystem.controllers
 
 import com.fourtk.creditapplicationsystem.dtos.CustomerDto
+import com.fourtk.creditapplicationsystem.dtos.CustomerView
+import com.fourtk.creditapplicationsystem.entities.Customer
 import com.fourtk.creditapplicationsystem.services.impl.CustomerService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,11 +17,19 @@ import org.springframework.web.bind.annotation.RestController
 class CustomerController(
     private val customerService: CustomerService
 ) {
-
     @PostMapping
-    fun saveCustomer(@RequestBody customerDto: CustomerDto): String{
+    fun saveCustomer(@RequestBody customerDto: CustomerDto): String {
         val saveCustomer = customerService.save(customerDto.toEntity())
         return "Customer ${saveCustomer.email} saved!"
     }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerView {
+        val customer: Customer = customerService.findById(id)
+        return CustomerView(customer)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable id: Long) = customerService.delete(id)
 
 }
---
+++ credit-application-system/src/main/kotlin/com/fourtk/creditapplicationsystem/dtos/CustomerView.kt
@@ -0,0 +1,24 @@
+package com.fourtk.creditapplicationsystem.dtos
+
+import com.fourtk.creditapplicationsystem.entities.Customer
+import java.math.BigDecimal
+
+data class CustomerView(
+    val firstName: String,
+    val lastName: String,
+    val cpf: String,
+    val email: String,
+    val income: BigDecimal,
+    val zipCode: String,
+    val street: String
+){
+    constructor(customer: Customer): this(
+        firstName = customer.firstName,
+        lastName = customer.lastName,
+        cpf = customer.cpf,
+        income = customer.income,
+        email = customer.email,
+        zipCode = customer.address.zipCode,
+        street = customer.address.street
+    )
+}
