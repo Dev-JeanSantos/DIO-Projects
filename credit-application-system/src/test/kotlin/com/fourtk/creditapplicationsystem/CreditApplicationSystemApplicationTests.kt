c498a872bbbb83510a58e4999c8ece8a6476a7b9
Dev-JeanSantos <jeancbsan@gmail.com>
2023-06-01 13:19:40 -0300

    all challenges of DIO

---
+++ credit-application-system/src/test/kotlin/com/fourtk/creditapplicationsystem/CreditApplicationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fourtk.creditapplicationsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditApplicationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
