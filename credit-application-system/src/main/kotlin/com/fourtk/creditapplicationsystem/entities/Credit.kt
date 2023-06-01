aee31b7ebd63237ebf09f8ab6506f09265c5fdf1
Dev-JeanSantos <jeancbsan@gmail.com>
2023-06-01 19:15:36 -0300

    Implemented mapper jpa of entities

--- credit-application-system/src/main/kotlin/com/fourtk/creditapplicationsystem/entities/Credit.kt
+++ credit-application-system/src/main/kotlin/com/fourtk/creditapplicationsystem/entities/Credit.kt
@@ -1,16 +1,23 @@
 package com.fourtk.creditapplicationsystem.entities
 
 import com.fourtk.creditapplicationsystem.enums.Status
+import jakarta.persistence.*
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.UUID
 
+@Entity
+@Table(name = "tb_credit")
 data class Credit (
-    val creditCode: UUID = UUID.randomUUID(),
-    val creditValue: BigDecimal =  BigDecimal.ZERO,
-    val dayFirstInstallment: LocalDate,
-    val numberOfInstallments: Int = 0,
+    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false) val creditValue: BigDecimal =  BigDecimal.ZERO,
+    @Column(nullable = false) val dayFirstInstallment: LocalDate,
+    @Column(nullable = false) val numberOfInstallments: Int = 0,
+    @Enumerated(EnumType.STRING)
     val status: Status = Status.IN_PROGRESS,
+    @ManyToOne
     val customer: Customer? =  null,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
 )
--- credit-application-system/src/main/kotlin/com/fourtk/creditapplicationsystem/entities/Customer.kt
+++ credit-application-system/src/main/kotlin/com/fourtk/creditapplicationsystem/entities/Customer.kt
@@ -1,12 +1,26 @@
 package com.fourtk.creditapplicationsystem.entities
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "tb_customer")
 data class Customer(
-    var firstName: String = "",
-    var lastName: String = "",
-    val cpf: String,
-    var email: String = "",
-    var password: String = "",
+    @Column(nullable = false) var firstName: String = "",
+    @Column(nullable = false) var lastName: String = "",
+    @Column(nullable = false, unique = true) val cpf: String,
+    @Column(nullable = false, unique = true) var email: String = "",
+    @Column(nullable = false) var password: String = "",
+    @Column(nullable = false)
+    @Embedded
     var address: Address,
+    @Column(nullable = false)
+    @OneToMany(fetch = FetchType.LAZY, cascade = arrayOf(
+        CascadeType.REMOVE,
+        CascadeType.PERSIST
+    ),  mappedBy = "customer")
     var credits: List<Credit> = mutableListOf(),
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
 )
